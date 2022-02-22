3350ab48d0b9b8e060d8ec084b9b5b24d0d8255e
Robert Wiebe <robert.wiebe9@icloud.com>
2022-02-21 17:51:03 -0800

    Initial Commit

---
+++ keyword item saver/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  keyword item saver
+//
+//  Created by Robert Wiebe on 2/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ keyword item saver/keyword_item_saverApp.swift
@@ -0,0 +1,17 @@
+//
+//  keyword_item_saverApp.swift
+//  keyword item saver
+//
+//  Created by Robert Wiebe on 2/21/22.
+//
+
+import SwiftUI
+
+@main
+struct keyword_item_saverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
